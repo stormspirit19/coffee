931ecc2502d0c0cdd5f2b22223dbe556249875b1
stormspirit19 <stormspirit1904@gmail.com>
2024-08-05 09:39:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let slides = document.querySelectorAll('.slide')
+
+function slideAnimate(active) {
+    slides[active].classList.add('active')
+    
+    slides.forEach((slide) => {
+    slide.addEventListener('click',() =>{
+        clearActive();
+        slide.classList.add('active');
+    })
+    })
+}
+
+
+
+function clearActive (){
+slides.forEach((item) => item.classList.remove('active'))
+}
+
+
+
+
+slideAnimate(0)
+clearActive(0)
